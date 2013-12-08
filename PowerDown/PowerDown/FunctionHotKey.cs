bb1801aeb80d63cfa5a9972214e69a969b5dde13
glen-nicol <glen.nicol@live.com>
2013-12-07 23:27:04 -0800

    trying to get global hotkeys working.

---
+++ PowerDown/PowerDown/FunctionHotKey.cs
@@ -0,0 +1,47 @@
+﻿using PowerDown.HDLibrary.Wpf.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PowerDown
+{
+	[Serializable]
+	public class FunctionHotKey : HotKey
+	{
+		private Action _function;
+		public FunctionHotKey(Action f, Key key, ModifierKeys modifiers, bool enabled)
+			: base(key, modifiers, enabled)
+		{
+			_function = f;
+		}
+
+		private string name;
+		public string Name
+		{
+			get { return name; }
+			set
+			{
+				if (value != name)
+				{
+					name = value;
+					OnPropertyChanged(name);
+				}
+			}
+		}
+
+		protected override void OnHotKeyPress()
+		{
+			_function.Invoke();
+		}
+
+		public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+		{
+			base.GetObjectData(info, context);
+			info.AddValue("function", _function.GetHashCode());
+		}
+
+	}
+}
--- PowerDown/PowerDown/MainWindow.xaml.cs
+++ PowerDown/PowerDown/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,20 +13,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using PowerDown.HDLibrary.Wpf.Input;
+using System.Windows.Interop;
 
 namespace PowerDown
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window 
 	{
+		private HotKey OpenCloseToggle;
+		public bool Open { get; private set; }
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			Width = System.Windows.SystemParameters.PrimaryScreenWidth;
 			Left = 0;
 			Top = 0;
+			Open = true;
+
+			Loaded += Window_Loaded;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			HotKeyHost hotKeyHost = new HotKeyHost((HwndSource)HwndSource.FromVisual(App.Current.MainWindow));
+			hotKeyHost.AddHotKey(new FunctionHotKey(ToggleOpen ,Key.F10, ModifierKeys.Control | ModifierKeys.Shift, true));
+
+		}
+
+		private void ToggleOpen()
+		{
+			if (Open)
+			{
+				Height = 0;
+			}
+			else
+			{
+				Height = 500;
+			}
+			Open = !Open;
 		}
 	}
 }
